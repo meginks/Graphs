1d596b77ee925a09efa3dd4ebbc4950a7f51fd47
meginks <meganjones.dev@gmail.com>
2019-10-07 12:55:16 -0500

    breadth first traversal done

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -11,18 +11,33 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex] = set()
+
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2) 
+        else: 
+            raise IndexError("That vertex doesn't exist")
+
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        queue = Queue() 
+        visited = set() 
+        queue.enqueue(starting_vertex) 
+        while queue.size() > 0: 
+            vertex = queue.dequeue() 
+            if vertex not in visited: 
+                visited.add(vertex) 
+                print(vertex) 
+                for next_vert in self.vertices[vertex]:
+                    queue.enqueue(next_vert)
+      
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
