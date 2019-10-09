c96c358de8d367e0796091854789631787833e1c
meginks <meganjones.dev@gmail.com>
2019-10-09 12:48:03 -0500

    islands from class

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,38 +1,82 @@
-from collections import defaultdict
-class Stack():
-    def __init__(self):
-        self.stack = []
-    def push(self, value):
-        self.stack.append(value)
-    def pop(self):
-        if self.size() > 0:
-            return self.stack.pop()
-        else:
-            return None
-    def size(self):
-        return len(self.stack)
+# from collections import defaultdict
+# class Stack():
+#     def __init__(self):
+#         self.stack = []
+#     def push(self, value):
+#         self.stack.append(value)
+#     def pop(self):
+#         if self.size() > 0:
+#             return self.stack.pop()
+#         else:
+#             return None
+#     def size(self):
+#         return len(self.stack)
         
-def dfs(starting_vertex, ancestors):
-    visited = []
-    stack = Stack()
-    stack.push([starting_vertex])
-    while stack.size() > 0:
-        path = stack.pop()
-        vertex = path[-1]
-        if vertex not in visited:
-            visited.append(vertex)
-        for neighbor in ancestors[vertex]:
-            path_copy = [*path]
-            path_copy.append(neighbor)
-            stack.push(path_copy)
-    return visited[-1]
+# def dfs(starting_vertex, ancestors):
+#     visited = []
+#     stack = Stack()
+#     stack.push([starting_vertex])
+#     while stack.size() > 0:
+#         path = stack.pop()
+#         vertex = path[-1]
+#         if vertex not in visited:
+#             visited.append(vertex)
+#         for neighbor in ancestors[vertex]:
+#             path_copy = [*path]
+#             path_copy.append(neighbor)
+#             stack.push(path_copy)
+#     return visited[-1]
 
+# def earliest_ancestor(ancestors, starting_node):
+#     family = defaultdict(list)
+#     for parent, child in ancestors:
+#         family[child].append(parent)
+#     if starting_node not in family:
+#         return -1
+#     earliest_ancestor = dfs(starting_node, family)
+#     return earliest_ancestor
+class Queue():
+  def __init__(self):
+    self.queue = []
+  def enqueue(self, value):
+    self.queue.append(value)
+  def dequeue(self):
+    if self.size() > 0:
+      return self.queue.pop(0)
+    else:
+      return None
+  def size(self):
+    return len(self.queue)
+class Graph:
+  def __init__(self):
+    self.vertices = {}
+  
+  def add_vertex(self, vertex_id):
+    if vertex_id not in self.vertices:
+      self.vertices[vertex_id] = set()
+  def add_edges(self, v1, v2):
+    if v1 in self.vertices and v2 in self.vertices:
+      self.vertices[v1].add(v2)
+    else:
+      raise IndexError("That vertex does not exist!")
 def earliest_ancestor(ancestors, starting_node):
-    family = defaultdict(list)
-    for parent, child in ancestors:
-        family[child].append(parent)
-    if starting_node not in family:
-        return -1
-    earliest_ancestor = dfs(starting_node, family)
-    return earliest_ancestor
-
+  graph = Graph()
+  for pair in ancestors:
+    graph.add_vertex(pair[0])
+    graph.add_vertex(pair[1])
+    graph.add_edges(pair[1], pair[0])
+  queue = Queue()
+  queue.enqueue([starting_node])
+  max_path_length = 1
+  earliest_ancestor = -1
+  while queue.size() > 0:
+    path = queue.dequeue()
+    v = path[-1]
+    if (len(path) >= max_path_length and v < earliest_ancestor) or (len(path) > max_path_length):
+      earliest_ancestor = v
+      max_path_length = len(path)
+    for neighbor in graph.vertices[v]:
+      path_copy = list(path)
+      path_copy.append(neighbor)
+      queue.enqueue(path_copy)
+  return earliest_ancestor
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -74,14 +74,39 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        queue = Queue() 
+        visited = set() 
+        queue.enqueue([starting_vertex]) 
+        while queue.size() > 0:
+            path = queue.dequeue() 
+            vertex = path[-1]
+            if vertex not in visited: 
+                return path 
+            visited.add(vertex)
+            for next_vert in self.vertices[vertex]: 
+                new_path = list(path)
+                new_path.append(next_vert) 
+                queue.enqueue(new_path)
+
     def dfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        stack = Stack() 
+        visited = set() 
+        while stack.size() > 0:
+            path = stack.pop() 
+            vertex = path[-1] 
+            if vertex not in visited:
+                if vertex == destination_vertex: 
+                    return path 
+            visited.add(vertex)
+            for next_vert in self.vertices[vertex]: 
+                new_path = list(path)
+                new_path.append(next_vert) 
+                stack.push(new_path)
 
 
 
